d45273430f1c4caf71060ffb13bc110296437079
achen889 <albertc@smu.edu>
2016-01-11 15:51:25 -0600

    My Assignment 1 Program
    First Commit to test the GitHub system for Albert Chen

---
+++ sd6/sd6_a1/sd6_a1.cpp
@@ -0,0 +1,17 @@
+// sd6_a1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int main(int argc, _TCHAR* argv[])
+{
+
+	printf("Albert says Game Over\n");
+
+	system("pause");
+
+	return 0;
+}
+
